d59170c0bea16c8a49936a48f2c885992a1e593a
Ghameerah (Juh-meer-ruh) McCullers <59162870+ameerrah9@users.noreply.github.com>
2023-07-28 11:01:36 -0400

    Create kitten routes & controller actions

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,2 +1,61 @@
 class KittensController < ApplicationController
+
+  def index
+    @kittens = Kitten.all
+    respond_to do |format|
+      format.html
+      format.json { render :json => @kittens }
+    end
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render :json => @kitten }
+    end
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.create(kitten_params)
+    if @kitten.save
+      flash[:success] = "Kitten created!"
+      redirect_to @kitten
+    else
+      flash.now[:error] = "Kitten not created!"
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+    if @kitten.update_attributes(kitten_params)
+      flash[:success] = "Kitten updated!"
+      redirect_to @kitten
+    else
+      flash.now[:error] = "Kitten not updated!"
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+    flash[:success] = "Kitten deleted!"
+    redirect_to root_url
+  end
+
+  private
+
+    def kitten_params
+      params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+    end
+
 end
